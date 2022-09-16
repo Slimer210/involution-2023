79f80992287c679f85db365a1d6d014699874421
Slimer210 <howweishan0210@outlook.com>
2022-09-16 10:44:52 +0800

    Demo File Modification

--- demo.cpp
+++ demo.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
-#include <array>
-#include <algorithm>
-#include <vector>
-using namespace std;
  
 int main(){
-    vector<int>v;
-    vector<int>arr;
-    int x;
-    cin>>x;
-    while (x--) {
-        int i;
-        cin>>i;
-        v.push_back(i);
-        arr.push_back(i);
-    }
-    int check = *min_element(v.begin(), v.end());
-    int check2 = *max_element(v.begin(), v.end());
-    arr.erase(remove_if(arr.begin(),arr.end(),[check](int a){return a==check;}));
-    arr.erase(remove_if(arr.begin(),arr.end(),[check2](int a){return a==check2;}));
-    int max=count(v.begin(),v.end(),check2);
-    int min=count(v.begin(),v.end(),check);
-    for(int i=0;i<min;i++){
-        arr.insert(begin(arr),check);
-    }
-    for(int i=0;i<max;i++){
-        arr.push_back(check2);
-    }
-    for(int i=0;i<x;i++){
-        cout<<arr[i];
-    }
+    std::cout << "Hello World" << std::endl;
+    std::cout << "You are using ";
+    if (__cplusplus == 201703L) std::cout << "C++ 17\n";
+    else if (__cplusplus == 201402L) std::cout << "C++ 14\n";
+    else if (__cplusplus == 201103L) std::cout << "C++ 11\n";
+    else if (__cplusplus == 199711L) std::cout << "C++ 98\n";
+    else std::cout << "pre-standard C++\n";
+    return 0;
 }
+
