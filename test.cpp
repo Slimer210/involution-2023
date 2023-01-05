8ed63e39f43cf9457c481660e55ce092c49aabcb
Slimer210 <howweishan0210@outlook.com>
2023-01-05 19:56:27 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
+#include <set>
 using namespace std;
 
 int main() {
     string inputString;
     getline(cin, inputString);
-    vector<string> stringList;
+    inputString.erase(remove(inputString.begin(), inputString.end(), ','), inputString.end());
+    inputString.erase(remove(inputString.begin(), inputString.end(), '.'), inputString.end());
+    inputString.erase(remove(inputString.begin(), inputString.end(), '?'), inputString.end());
+    transform(inputString.cbegin(), inputString.cend(), inputString.begin(), [](unsigned char c) { return std::toupper(c); });
+
+    set<string> stringList;
     string k = ""; // initializize an empty temp string
     for (auto i : inputString) {
         if (i==' ') {
-            cout << k << endl;
+            stringList.insert(k);
             k = "";
 
         } else {
             k=k+i;
         }
     }
-    cout << k << endl;
+    stringList.insert(k);
+    cout << stringList.size() << endl;
+    for (string x : stringList) {
+        cout << x << endl;
+    }
 }
