1dcd34d9aaa8586787943306599111f9810a4161
Slimer210 <howweishan0210@outlook.com>
2023-02-17 20:19:11 +0800

    class 4

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 int main() {
-    int a;
-    printf("Enter an integer\n");
-    scanf("%d", &a);
-    printf("Integer that you have entered is %d\n", a);
+    // printf("    I can\'t\b\b  \rYES");
+    printf("No, I can't");
+    printf("\b");
+    printf("\b");
+    printf("!");
+    printf("!");
+    printf("\r");
+    printf("YES");
+
     return 0;
 }
+
+// \b = backspace (move cursor to 1 character behind)
+// \r = return (move cursor to the first character in the line)
+// \n = newline
