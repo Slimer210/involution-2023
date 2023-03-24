8d23d91c38e481eb04558edc1f607ad219aea85b
Slimer210 <howweishan0210@outlook.com>
2023-03-24 22:21:56 +0800

    ex8 & ex10

---
+++ school-c-class/assignments/ex10.c
@@ -0,0 +1,11 @@
+// FOR REEFERENCE ONLY
+#include<stdio.h>
+int main() {
+    char c1; float x;
+    printf("请输入一个子元：\n");
+    scanf("%c", &c1);
+    printf("\n请输入一个浮点数：\n");
+    scanf("%f", &x);
+    printf("\nc1 = \'%c\'\nx = %.3f\n",c1,x);
+    return 0;
+}
