c3084962199013fc56743ae618bc67410f9e1703
Slimer210 <howweishan0210@outlook.com>
2023-03-24 22:21:56 +0800

    ex8 & ex10

--- school-c-class/assignments/ex8.c
+++ school-c-class/assignments/ex8.c
@@ -1,3 +1,4 @@
+// FOR REFERNCE ONLY
 #include<stdio.h>
 int main() {
     int a, b; float x, y; // 忘了declare所以在写在同一行
@@ -6,6 +7,6 @@ int main() {
     printf("\n请输入2个浮点数：\n");
     scanf("%f %f", &x, &y);
     printf("\n你所输入的资料为：\n");
-    printf("A\tB\tX\tY\n%d\t%d\t%.2f\t%.2f", a, b, x, y);
+    printf("A\tB\tX\tY\n%d\t%d\t%.2f\t%.2f\n", a, b, x, y);
     return 0;
 }
