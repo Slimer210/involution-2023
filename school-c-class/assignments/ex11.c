c5233fcc82aac372a816aed3b0e8828f2348586c
Slimer210 <howweishan0210@outlook.com>
2023-04-17 18:16:26 +0800

    File Migrations

--- school-c-class/assignments/ex11.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    float temp;
-    scanf("%f", &temp);
-    printf("%.0f", (temp-32)*5/9);
-    return 0;
-}
--- school-c-class/info.md
+++
@@ -1,4 +0,0 @@
-# Class Info
-Thursday, #4-5
-
-L1003
