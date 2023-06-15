7d832a03fa5a280c3a8a0f0906a2471956b82f00
侯韦杉 <66945259+Slimer210@users.noreply.github.com>
2023-06-15 11:08:20 +0800

    Update question6.cpp (finished)

--- NSSPC 2022 PRELIMINARY/question6.cpp
+++ NSSPC 2022 PRELIMINARY/question6.cpp
@@ -6,26 +6,30 @@
 using namespace std;
 
 int main() {
-	int m;
-	int c;
-	int ca = 1;
-	int cb = -2;
-	int cc = -3;
+	double m;
+	double c;
+	double ca = 1;
+	double cb = -2;
+	double cc = -3;
 	cin >> m >> c;
-	cb+=m;
-	cc+=c;
+	cb-=m;
+	cc-=c;
 	double det = (cb*cb)-(4*ca*cc);
 	if (det<0) {
-		cout << "No intersections" << endl;
+		cout << "No intersection" << endl;
 		return 0;
 	} else if(det==0) {
 		cout << "One intersection" << endl;
 		double x = ((-cb)+sqrt(det))/(2*ca);
-		printf("%.7lf\n", x);
+		
+		printf("%.7lf, %.7lf\n", x, (x*x + -2*x -3));
 	} else {
 		cout << "Two intersections" << endl;
 		double x1 = ((-cb)+sqrt(det))/(2*ca);
+		double y1 = x1*x1 + -2*x1 -3;
 		double x2 = ((-cb)-sqrt(det))/(2*ca);
-		printf("%.7lf\n%.7lf\n", x1, x2);
+		double y2 = x2*x2 + -2*x2 -3;
+		printf("%.7lf, %.7lf\n", x1, y1);
+		printf("%.7lf, %.7lf\n", x2, y2);
 	}
 }
