b000e971fe57ac6bacbbb24ccad5f5df9a080289
Slimer210 <howweishan0210@outlook.com>
2023-08-05 22:44:59 +0800

    Backups

--- test.c
+++ test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+int main(){
+	
+	int a,b,c,d;
+	
+	printf("Enter grade respectively (BM,BI,BC,MC):");
+	scanf("%d %d %d %d",&a,&b,&c,&d);
 
-int main() {
-    printf("Hello World!\n");
-    return 0;
+    if(a>40 && b>40 && c>40 && d>40)
+	printf("Pass");
+    else
+    printf("Fail");
+		
+	return 0;
+	
 }
