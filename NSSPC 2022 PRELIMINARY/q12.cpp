6c8ee1ddd89f40767a9b8c6cf898170307901632
Slimer210 <howweishan0210@outlook.com>
2023-08-21 22:56:16 +0800

    21-8-2023

---
+++ NSSPC 2022 PRELIMINARY/q12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, x;
+    int a;
+    cin >> n >> x;
+    int temp=n;
+    vector<double> v;
+
+    while (temp--) {
+        cin >> a;
+        v.push_back(a);
+    }
+
+    
+}
