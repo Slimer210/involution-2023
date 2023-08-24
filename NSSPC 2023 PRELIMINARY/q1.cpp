b11d6429b39ff096f71438dbd8fdb94803760645
侯韦杉 <66945259+Slimer210@users.noreply.github.com>
2023-08-24 10:34:11 +0800

    Update q1.cpp

--- NSSPC 2023 PRELIMINARY/q1.cpp
+++ NSSPC 2023 PRELIMINARY/q1.cpp
@@ -1,1 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cin.tie(NULL);
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	int totalApples = c/d; 
+	totalApples += (totalApples / a) * b;
+	cout << totalApples << endl;
+	return 0;
+}
 
